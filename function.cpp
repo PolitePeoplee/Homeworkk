e30630b78a259af4bd06d2cfbdd2240432206093
PolitePeoplee <150465337+PolitePeoplee@users.noreply.github.com>
2024-03-10 17:28:39 +0300

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -2,6 +2,8 @@ pa﻿#include <iostream>
 #include "function.h"
 #include <cassert>
 
+namespace test
+{
 std::vector<int> map(std::vector<int> numbers, std::function<int(int)> fun)
 {
     std::vector<int> result;
@@ -22,16 +24,17 @@ std::vector<int> filter(std::vector<int> numbers, std::function<bool(int)> funn)
     }
     return result;
 }
+}
 
 void tests()
 {
     std::vector<int> test = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
     std::vector<int> massiv = { 1, 4, 9, 16, 25, 36, 49, 64, 81 };
-    assert(map(test, [](int x) {return x * x; })==massiv);
+    assert(test::map(test, [](int x) {return x * x; })==massiv);
 
     std::vector<int> tested = {  2,  4,  6, 8 };
-    assert(filter(test, [](int x) {if (x % 2 == 0) return true; else return false; })==tested);
+    assert(test::filter(test, [](int x) {if (x % 2 == 0) return true; else return false; })==tested);
 }
 int main()
 {
