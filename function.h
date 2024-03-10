23a9ae02f4a865e1ce79a375e6f9e3fdf7769c30
PolitePeoplee <150465337+PolitePeoplee@users.noreply.github.com>
2024-03-10 17:26:46 +0300

    Update function.h

--- function.h
+++ function.h
@@ -1,4 +1,7 @@
 #include <vector>
-#include<functional> // замена указателя на ф-ю
+#include<functional> 
+namespace test
+{
 std::vector<int> map(std::vector<int> numbers, std::function<int(int)>);
 std::vector<int> filter(std::vector<int> numbers, std::function<bool(int)>);
+}
